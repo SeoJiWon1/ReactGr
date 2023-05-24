b869092cf8b025aa83d2a300f9a259bbec1dabe7
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-05-24 23:06:52 +0900

    redux store

--- Redux예제/store.js
+++ Redux예제/store.js
@@ -1,12 +1,23 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-createSlice({
-    name: 'state이름~',
-    initialState : '값'
+let user = createSlice({
+    name: 'user',
+    initialState : 'kim'
+                    
+})
+
+let cart = createSlice({
+    name : 'cart',
+    initialState: [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ] 
 })
 
 
-
 export default configureStore({
-    reducer:{}
+    reducer:{
+        user : user.reducer,
+        cart : cart.reducer
+    }
 })
